9d21929d07e150bb49b35cc43f5b888ad6c89544
Sergio Matamala <sergio.matamala93@gmail.com>
2025-04-19 15:44:32 -0400

    lesson three

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -18,6 +19,28 @@ func About(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s", fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(100.0, 10.0)
+	if err != nil {
+		fmt.Fprintf(w, "cannot divide by 0")
+		return
+	}
+
+	fmt.Fprintf(w, "%s", fmt.Sprintf("%f divide by %f is %f", 100.0, 10.0, f))
+
+}
+
+func divideValues(x, y float32) (float32, error) {
+
+	if y == 0 {
+		err := errors.New("Cannot divide by zero")
+		return 0, err
+	}
+
+	result := x / y
+	return result, nil
+}
+
 func addValues(x, y int) int {
 	return x + y
 }
@@ -25,6 +48,7 @@ func addValues(x, y int) int {
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	// fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	fmt.Printf("Starting application on port %s \n", portNumber)
